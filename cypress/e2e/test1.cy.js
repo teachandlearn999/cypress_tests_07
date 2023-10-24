68822458a2cb60d1171e1da5f7ec634cd976b044
BorisS <javaforqes@gmail.com>
2023-10-23 19:21:45 -0700

    committing first set of tests

---
+++ cypress/e2e/test1.cy.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+
+describe("Test", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:8080/login?from=%2F");
+        cy.get("#j_username").type("admin");
+        cy.get('input[name="j_password"]').type("password");
+        cy.get('button[name="Submit"]').click();
+    });
+
+    it("Checking Logo", () => {
+        cy.get("#jenkins-name-icon").should("have.attr", "src").should("include", "title.svg");
+    });
+
+    it('Create a job', () => {
+        cy.get('a[href="/view/all/newJob"]').click();
+        cy.get('#name').type('Freestyle');
+        cy.get('.hudson_model_FreeStyleProject').click();
+        cy.get('#ok-button').click();
+        cy.get('button[name="Submit"]').click();
+        cy.get("#jenkins-name-icon").click();
+        cy.get('#job_Freestyle a[href="job/Freestyle/"] span').should('have.text', 'Freestyle');
+    });
+
+    it('Delete a job', () => {
+        cy.get('#job_Freestyle a[href="job/Freestyle/"]').click();
+        cy.get('a[data-url="/job/Freestyle/doDelete"]').click();
+        cy.get('#job_Freestyle a[href="job/Freestyle/"] span').should('not.exist');
+    });
+
+    // it('Delete a job2', () => {
+    //     cy.get('#job_Freestyle a[href="job/Freestyle/"] button').click({force: true});
+    //     cy.get('button[href="/job/Freestyle/doDelete"]').click();
+    //     cy.get('#job_Freestyle a[href="job/Freestyle/"] span').should('not.exist');
+    // });
+});
--- file1.js
+++
@@ -1,7 +0,0 @@
-function myFirstFunction() {
-	console.log("hello world");
-}
-
-function mySecondFunction() {
-	return "a";
-}
