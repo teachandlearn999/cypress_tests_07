8f8376ab9b328c70ff6df9ee826001b7a2f5bcaf
Anastasiia <58123026+AnastasiiaGri@users.noreply.github.com>
2023-10-26 16:27:15 -0700

    refactor: split test1 into 2 files (#8)
    * refactor: split test1 into 2 files
    
    * refactor: delete original file

--- cypress/e2e/testCreateAndDeleteJob.cy.js
+++ cypress/e2e/testCreateAndDeleteJob.cy.js
@@ -2,10 +2,6 @@
 
 describe("Cypress_Tests_07", () => {
 
-    it("Checking Logo", () => {
-        cy.get("#jenkins-name-icon").should("have.attr", "src").should("include", "title.svg");
-    });
-
     it('Create a job', () => {
         cy.get('a[href="/view/all/newJob"]').click();
         cy.get('#name').type('Freestyle');
@@ -15,16 +11,16 @@ describe("Cypress_Tests_07", () => {
         cy.get("#jenkins-name-icon").click();
         cy.get('#job_Freestyle a[href="job/Freestyle/"] span').should('have.text', 'Freestyle');
     });
-
+// version 1
     it('Delete a job', () => {
         cy.get('#job_Freestyle a[href="job/Freestyle/"]').click();
         cy.get('a[data-url="/job/Freestyle/doDelete"]').click();
         cy.get('#job_Freestyle a[href="job/Freestyle/"] span').should('not.exist');
     });
-
-    // it('Delete a job2', () => {
-    //     cy.get('#job_Freestyle a[href="job/Freestyle/"] button').click({force: true});
-    //     cy.get('button[href="/job/Freestyle/doDelete"]').click();
-    //     cy.get('#job_Freestyle a[href="job/Freestyle/"] span').should('not.exist');
-    // });
+//// version 2
+//     it('Delete a job2', () => {
+//         cy.get('#job_Freestyle a[href="job/Freestyle/"] button').click({force: true});
+//         cy.get('button[href="/job/Freestyle/doDelete"]').click();
+//         cy.get('#job_Freestyle a[href="job/Freestyle/"] span').should('not.exist');
+//     });
 });
