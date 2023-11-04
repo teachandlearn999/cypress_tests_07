fb67658e278a378fcea835a3a315783959a48f7c
Klavdia <kgold94@yahoo.com>
2023-11-03 21:55:16 -0700

    lg/create user form validation

--- cypress/e2e/testFormValidation.cy.js
+++ cypress/e2e/testFormValidation.cy.js
@@ -1,22 +1,27 @@
 /// <reference types="cypress" />
-describe("testFormValidation", () => {
+import manageJenkinsSecurityCreateUser from "../fixtures/manageJenkinsSecurityCreateUser.json";
+describe.only("testFormValidation", () => {
+  it("show validation error when username is blank ", function () {
+    cy.get("div:nth-child(4) > span > a").click();
+    cy.get('a[href="securityRealm/"]').click();
 
+    cy.title().should("contain", "Users");
 
-    it("show when username is blank validation error", function() {
-        cy.get('div:nth-child(4) > span > a').click();
-        cy.get('a[href="securityRealm/"]').click()
-        cy.get('div#page-body a[href="addUser"]').click()
-        //  // do not fill username
-        
-        cy.get('div.form-content input[name="username"]').clear().should('be.empty')
-        cy.get('input[name="password1"]').type('1234')
-        cy.get('input[name="password2"]').type('1234')
-        cy.get('input[name="fullname"]').type('FName')
-        cy.get('input[name="email"]').type('qa@qa.com')
-        cy.get('button[name="Submit"]').click()
-        cy.get('.error')
-        .should('be.visible')
-        .and('contain', 'is prohibited as a username for security reasons.')
-    });
-})
+    cy.get('div#page-body a[href="addUser"]').click();
 
+    cy.get('div.form-content input[name="username"]')
+      .clear()
+      .should("be.empty");
+    
+    cy.get('input[name="password1"]').type(
+      manageJenkinsSecurityCreateUser.userFormFields.password1);
+    cy.get('input[name="password2"]').type(manageJenkinsSecurityCreateUser.userFormFields.password2);
+    cy.get('input[name="fullname"]').type(manageJenkinsSecurityCreateUser.userFormFields.fullname);
+    cy.get('input[name="email"]').type(manageJenkinsSecurityCreateUser.userFormFields.email);
+    cy.get('button[name="Submit"]').click();
+   
+    cy.get(".error")
+      .should("be.visible")
+      .and("contain", manageJenkinsSecurityCreateUser.userFormFields.errorNoUserName);
+  });
+});
