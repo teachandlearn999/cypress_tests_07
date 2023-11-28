d0805a9cac48a7f9e00db966e803a60fc020e0d4
Anastasiia <58123026+AnastasiiaGri@users.noreply.github.com>
2023-11-28 07:29:42 -0800

    VerifyHelpIconsNamesPom (#23)

--- cypress/pageObjects/FreestyleProjectConfigurePage.js
+++ cypress/pageObjects/FreestyleProjectConfigurePage.js
@@ -2,11 +2,13 @@ import FreestyleProjectStatusPage from "./FreestyleProjectStatusPage";
 
 class FreestyleProjectConfigurePage {
     getSaveButton = () => cy.get('button[name="Submit"]');
-
-
+    getBuildTriggers = () => cy.get("#build-triggers").parent().find('.optionalBlock-container');
+    getIndividualBuildTrigger = '.optional-block-start label.attach-previous';
+    
     clickSaveButton () {
         this.getSaveButton ().click();
         return new FreestyleProjectStatusPage();
     }
+
 }
 export default FreestyleProjectConfigurePage;
--- cypress/pageObjects/FreestyleProjectStatusPage.js
+++ cypress/pageObjects/FreestyleProjectStatusPage.js
@@ -1,4 +1,12 @@
+import HomePage from "./HomePage";
+
 class FreestyleProjectStatusPage{
+    getDeleteProjectNameLink = (name) => cy.get(name)
+
+    clickDeleteProjectNameLink (name) {
+        this.getDeleteProjectNameLink(name).click();
+        return new HomePage()
+    }
     
 }
 export default FreestyleProjectStatusPage;
